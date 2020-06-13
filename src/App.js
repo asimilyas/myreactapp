6e8c24f75accc1519851292429202f6a29e178bf
asimilyas <ilyas.aasim@gmail.com>
2020-06-13 18:55:11 +0500

    link removed

--- src/App.js
+++ src/App.js
@@ -5,20 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           This is Asim Ilyas first React Application
         </p>
-        <a
-          className="App-link"
-          href="http://asimilyas.surge.sh"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
